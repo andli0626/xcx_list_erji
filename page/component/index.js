47e370d914e2a94f9ebc94aa859f000c06f6f34e
李林 <ww1095@163.com>
2017-07-25 19:00:31 +0800

    二级联动列表的实现

---
+++ page/component/index.js
@@ -0,0 +1,66 @@
+Page({
+  data: {
+    list: [
+      {
+        id: 'view',
+        name: '视图容器',
+        open: false,
+        pages: ['view', 'scroll-view', 'swiper']
+      }, {
+        id: 'content',
+        name: '基础内容',
+        open: false,
+        pages: ['text', 'icon', 'progress']
+      }, {
+        id: 'form',
+        name: '表单组件',
+        open: false,
+        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch', 'textarea']
+      }, {
+        id: 'nav',
+        name: '导航',
+        open: false,
+        pages: ['navigator']
+      }, {
+        id: 'media',
+        name: '媒体组件',
+        open: false,
+        pages: ['image', 'audio', 'video']
+      }, {
+        id: 'map',
+        name: '地图',
+        pages: ['map']
+      }, {
+        id: 'canvas',
+        name: '画布',
+        pages: ['canvas']
+      }
+    ]
+  },
+  // 点击：一级列表项
+  clicklist1: function (e) {
+    var id    = e.currentTarget.id;
+    var list  = this.data.list;
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list: list
+    });
+  },
+  // 点击：二级列表项
+  clicklist2:function(e){
+
+    var index = e.currentTarget.dataset.index; // 下标
+    var name  = e.currentTarget.dataset.name;  // pagename
+
+    wx.showToast({
+      title: name,
+    })
+  }
+})
+
